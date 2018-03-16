910952ee067c4e41fda130fbc5a44ebe3d64f8e4
nzbin <nzb329@163.com>
2018-03-16 16:27:44 +0800

    添加省市区相关代码注释

--- example/js/cityPicker.js
+++ example/js/cityPicker.js
@@ -1,6 +1,7 @@
 /**
  *
  * cityPicker.js
+ *
  * cityPicker is made by Framework picker component which effects like iOS native select.
  *
  * nzb329@163.com
@@ -11,111 +12,110 @@
 ;
 (function(window, $, undefined) {
 
-    /**
-     * [getProvince 获取省]
-     * @param  {[Object]} regions [省市区数据]
-     * @return {[Array]}          [省数组]
-     */
-    function getProvince(regions) {
+  /**
+   * [getProvince 获取省]
+   * @param  {[Object]} regions [省市区数据]
+   * @return {[Array]}          [省数组]
+   */
+  function getProvince(regions) {
+    return regions['provincesArr'];
+  }
 
-        return regions['provincesArr'];
-    }
+  /**
+   * [getCity 获取市]
+   * @param  {[Object]} regions      [省市区数据]
+   * @param  {[String]} provinceName [省名]
+   * @return {[Array]}               [市数组]
+   */
+  function getCity(regions, provinceName) {
+    return regions['provinces'][provinceName]['citiesArr'];
+  }
 
-    /**
-     * [getCity 获取市]
-     * @param  {[Object]} regions      [省市区数据]
-     * @param  {[String]} provinceName [省名]
-     * @return {[Array]}               [市数组]
-     */
-    function getCity(regions, provinceName) {
+  /**
+   * [getArea 获取区]
+   * @param  {[Object]} regions      [省市区数据]
+   * @param  {[String]} provinceName [省名]
+   * @param  {[String]} cityName     [市名]
+   * @return {[Array]}               [区数组]
+   */
+  function getArea(regions, provinceName, cityName) {
+    return regions['provinces'][provinceName]['cities'][cityName]['areasArr'];
+  }
 
-        return regions['provinces'][provinceName]['citiesArr'];
-    }
+  // 初始化 Framework7
+  var myApp = new Framework7();
 
-    /**
-     * [getArea 获取区]
-     * @param  {[Object]} regions      [省市区数据]
-     * @param  {[String]} provinceName [省名]
-     * @param  {[String]} cityName     [市名]
-     * @return {[Array]}               [区数组]
-     */
-    function getArea(regions, provinceName, cityName) {
+  // 初始化省市区
+  var province = getProvince(regions),
+    city = getCity(regions, '北京市'),
+    area = getArea(regions, '北京市', '北京市');
 
-        return regions['provinces'][provinceName]['cities'][cityName]['areasArr'];
+  // 保存 picker 选择的省
+  var provinceSelect = '';
 
-    }
-
-    // 初始化 Framework7
-    var myApp = new Framework7();
-    var s = new Date();
-
-    // 初始化省市区
-    var province = getProvince(regions),
-        city = getCity(regions, '北京市'),
-        area = getArea(regions, '北京市', '北京市');
-    console.log(getArea(regions, '广东省', '中山市') == 0)
-    var e = new Date();
-    console.log(e - s);
-    // 保存 picker 选择的省
-    var provinceSelect = '';
-
-    // 省市区联动 / Framework7 picker
-    var pickerLocation = myApp.picker({
-        input: '#location',
-        rotateEffect: true,
-        toolbarTemplate: '<div class="toolbar">\
-                            <div class="toolbar-inner">\
-                                <div class="left">\
-                                    <a href="#" class="link close-picker">取消</a>\
-                                </div>\
-                                <div class="right">\
-                                    <a href="#" class="link close-picker">完成</a>\
-                                </div>\
-                            </div>\
-                        </div>',
-        cols: [{
-                cssClass: 'f-s-14',
-                width: '33.33%',
-                textAlign: 'left',
-                values: province,
-                onChange: function(picker, province) {
-                    if (picker.cols[1].replaceValues) {
-                        provinceSelect = province;
-                        city = getCity(regions, province);
-                        area = getArea(regions, province, city[0]);
-                        picker.cols[1].replaceValues(city);
-                        if (area.length !== 0) {
-                            picker.cols[2].replaceValues(area);
-                        } else {
-                            picker.cols[2].replaceValues(city);
-                        }
-                    }
-                }
-            },
-            {
-                cssClass: 'f-s-14',
-                width: '33.33%',
-                textAlign: 'center',
-                values: city,
-                onChange: function(picker, city) {
-                    if (picker.cols[2].replaceValues) {
-                        area = getArea(regions, provinceSelect, city);
-                        if (area.length !== 0) {
-                            picker.cols[2].replaceValues(area);
-                        } else {
-                            picker.cols[2].replaceValues([city]);
-                        }
-                    }
-                }
-            },
-            {
-                cssClass: 'f-s-14',
-                width: '33.33%',
-                textAlign: 'right',
-                values: area,
+  // 省市区联动 / Framework7 picker
+  var pickerLocation = myApp.picker({
+    input: '#location',
+    rotateEffect: true,
+    toolbarTemplate: '<div class="toolbar">\
+                          <div class="toolbar-inner">\
+                              <div class="left">\
+                                  <a href="#" class="link close-picker">取消</a>\
+                              </div>\
+                              <div class="right">\
+                                  <a href="#" class="link close-picker">完成</a>\
+                              </div>\
+                          </div>\
+                      </div>',
+    cols: [
+      // 省相关
+      {
+        cssClass: 'f-s-14',
+        width: '33.33%',
+        textAlign: 'left',
+        values: province,
+        onChange: function(picker, province) {
+          if (picker.cols[1].replaceValues) {
+            provinceSelect = province;
+            city = getCity(regions, province);
+            area = getArea(regions, province, city[0]);
+            picker.cols[1].replaceValues(city);
+            // 区相关
+            if (area.length !== 0) {
+              picker.cols[2].replaceValues(area);
+            } else {
+              picker.cols[2].replaceValues(city);
             }
-        ]
-    });
+          }
+        }
+      },
+      // 市相关
+      {
+        cssClass: 'f-s-14',
+        width: '33.33%',
+        textAlign: 'center',
+        values: city,
+        // 区相关
+        onChange: function(picker, city) {
+          if (picker.cols[2].replaceValues) {
+            area = getArea(regions, provinceSelect, city);
+            if (area.length !== 0) {
+              picker.cols[2].replaceValues(area);
+            } else {
+              picker.cols[2].replaceValues([city]);
+            }
+          }
+        }
+      },
+      // 区相关
+      {
+        cssClass: 'f-s-14',
+        width: '33.33%',
+        textAlign: 'right',
+        values: area,
+      }
+    ]
+  });
 
 
 })(window, jQuery);
